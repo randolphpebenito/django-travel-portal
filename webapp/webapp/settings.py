80fb07b1b47844f180036f3a42fe19c4778df7d2
Randolph Pe Benito <rpebenito@xyber.ph>
2017-03-23 15:23:09 +0800

    Updated ALLOWED_HOSTS

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a22tg0j0#(_2ey3sg!k5mt(-0llekh566k2ljv01069g^=e))k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
